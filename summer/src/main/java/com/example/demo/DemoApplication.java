959cff2a093d2d589455720674baddfe83a3e1ef
xiankunpeng <xian137>
2019-03-11 21:48:37 +0800

    myfiest

--- summer/src/main/java/com/example/demo/DemoApplication.java
+++ summer/src/main/java/com/example/demo/DemoApplication.java
@@ -7,8 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		//
+		//aaa
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("aaa");
 	}
 
 }
